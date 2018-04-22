bc3541c2a696f94339eaf9dc17c261a376390a64
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2018-04-22 15:33:40 +0200

    Update

--- src/projectile/Controller.java
+++ src/projectile/Controller.java
@@ -1,7 +1,48 @@
 package projectile;
 
+import javafx.animation.AnimationTimer;
+import javafx.beans.property.StringProperty;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
 public class Controller
 {
+    private Model model;
+    @FXML
+    private Pane drawingPane;
+    @FXML
+    private TextField angle, initialSpeed, initialHeight, distanceCovered, currentHeight;
+    private Circle object;
+    private class Timer extends AnimationTimer
+    {
+
+        @Override
+        public synchronized void handle(long now)
+        {
+                try
+                {
+                    double dt = model.get_dt();
+                    wait((long) (1000 * dt));
+                    model.update();
+                    updateAnimation();
+                    if(model.getCurrentHeight() > 400)
+                        this.stop();
+                }
+                catch (InterruptedException ex)
+                {
+
+                }
+            }
+        }
+
+
+    public void init()
+    {
+    }
+
     public void changeDrawingMode()
     {
 
@@ -10,6 +51,30 @@ public class Controller
     public void startSimulation()
     {
 
+        double initSpeed = Double.parseDouble(initialSpeed.getText());
+        double initHeight = Double.parseDouble(initialHeight.getText());
+        double initAngle = Double.parseDouble(angle.getText());
+        model = new Model(400 - initHeight, initAngle, initSpeed);
+        Circle object = new Circle(0, model.getInitialHeight(), 5);
+        drawingPane.getChildren().add(object);
+        this.object = object;
+        Timer timer = new Timer();
+        timer.start();
+
+
+
     }
+
+    public void updateAnimation()
+    {
+        object.setCenterX(model.getCurrentDisplacement());
+        object.setCenterY(model.getCurrentHeight());
+        int distInfo = (int)model.getCurrentDisplacement();
+        String text1 = Integer.toString(distInfo);
+        distanceCovered.setText(text1);
+        //currentHeight.setText(Integer.toString((int)model.getCurrentHeight()));
+    }
+
+
 
 }
--- src/projectile/Model.java
+++ src/projectile/Model.java
@@ -2,4 +2,60 @@ package projectile;
 
 public class Model
 {
+    private final double initialHeight, angle, initialSpeed, speedX;
+    private final double ANIMATION_FPS = 60;
+    private final double dt;
+    private final double gravitationalAcceleration = 10;
+    private double currentDisplacement, currentHeight, speedY;
+
+
+    public double getCurrentDisplacement()
+    {
+        return currentDisplacement;
+    }
+
+    public double getCurrentHeight()
+    {
+        return currentHeight;
+    }
+    public double getInitialHeight()
+    {
+        return initialHeight;
+    }
+
+    public double getAngle()
+    {
+        return angle;
+    }
+
+    public double getInitialSpeed()
+    {
+        return initialSpeed;
+    }
+
+    public double get_dt()
+    {
+        return this.dt;
+    }
+
+    public void update()
+    {
+        currentDisplacement = currentDisplacement + speedX * dt;
+        currentHeight = currentHeight + speedY * dt;
+        speedY = speedY + gravitationalAcceleration * dt;
+    }
+
+    public Model(double initialHeight, double angle, double initialSpeed)
+    {
+        this.initialHeight = initialHeight;
+        this.angle = angle;
+        this.initialSpeed = initialSpeed;
+        this.speedX = initialSpeed * Math.cos(angle*3.14/180);
+        this.speedY = -initialSpeed * Math.sin(angle*3.14/180);
+        this.dt = (1/ANIMATION_FPS);
+        this.currentHeight = initialHeight;
+        this.currentDisplacement = 0;
+
+    }
+
 }
